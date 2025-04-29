0f5bb9c8436a6e0a7a297c4ae7d4398119dee935
Osterberg, Hendrik <h.osterberg@DT-OSTERBERG>
2025-04-29 12:31:05 +0200

    Fix icon and script paths in index.html; format JSX in main.tsx for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext'; // Import ThemeProvider
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </StrictMode>,
 );
